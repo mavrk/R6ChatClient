49300f1cb53b3b538593e58e079a80a066105d83
Sanatt Abrol <sanatt.abrol.in@gmail.com>
2016-09-01 19:43:21 +0530

    Add files via upload

---
+++ src/ChatMessage.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sanatt Abrol
+ */
+
+import java.io.*;
+public class ChatMessage implements Serializable {
+
+	protected static final long serialVersionUID = 1112122200L;
+
+	// The different types of message sent by the Client
+	// WHOISIN to receive the list of the users connected
+	// MESSAGE an ordinary message
+	// LOGOUT to disconnect from the Server
+        // SVREQ to request a server
+        // SVBCAST to broadcast the server details
+	static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2, SVREQ = 3, SVBCAST = 4;
+	private int type;
+	private String message;
+	
+	// constructor
+	ChatMessage(int type, String message) {
+		this.type = type;
+		this.message = message;
+	}
+	
+	// getters
+	int getType() {
+		return type;
+	}
+	String getMessage() {
+		return message;
+	}
+}
+
